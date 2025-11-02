a876d00cf2942669a1e008db631ea4e61583b7aa
Abrha Gebrehiwet <abrishg.tesfamichael@gmail.com>
2025-11-02 10:31:11 +0300

    modified image.py

--- image.py
+++ image.py
@@ -1,7 +1,13 @@
 import cv2
-
+import matplotlib.pyplot as plt
 img = cv2.imread("image.jpg")
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+def cv2_imshow(img):
+   # img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    grayscale_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(grayscale_img, 128, 100, cv2.THRESH_BINARY)
+    plt.imshow(thresh)
+    plt.axis('off')
+    plt.show()
+cv2_imshow(img)
 
-
+ 
